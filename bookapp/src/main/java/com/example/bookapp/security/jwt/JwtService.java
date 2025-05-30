88aa698ae75a306659c92a7dd5658a8c6d34b5d4
Abel Camilo Yi Martínez <abelyimartinez1987@hotmail.com>
2025-05-30 16:07:53 -0500

    jwt expiration

--- bookapp/src/main/java/com/example/bookapp/security/jwt/JwtService.java
+++ bookapp/src/main/java/com/example/bookapp/security/jwt/JwtService.java
@@ -39,7 +39,7 @@ public class JwtService {
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24)) // 24h
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 3)) // 3 min
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
